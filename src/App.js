984284681de0ca72dc407eecacc621e9e4446e2d
Neenus Gabriel <hello@neenus.com>
2021-06-25 15:29:55 -0400

    feat(app.js footer.component.jsx): add footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import EmployeeForm from "./components/form/employee-form.component";
+import Footer from "./components/footer/footer.component";
 
 function App() {
   return (
     <div className="App">
       <h1>Time Sheet App</h1>
       <EmployeeForm />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/footer/footer.component.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import Link from "@material-ui/core/Link";
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary">
+      {"Copyright © "}
+      <Link color="inherit" href="https://material-ui.com/">
+        wwww.nraccounting.ca
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: "flex",
+    flexDirection: "column"
+    // minHeight: "100vh"
+  },
+  main: {
+    marginTop: theme.spacing(8),
+    marginBottom: theme.spacing(2)
+  },
+  footer: {
+    width: "100%",
+    padding: theme.spacing(3, 2),
+    marginTop: "5rem"
+  }
+}));
+
+export default function StickyFooter() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <footer className={classes.footer}>
+        <Container maxWidth="sm">
+          <Typography variant="body1">
+            NR Accounting & Business Advisors Inc.
+          </Typography>
+          <Copyright />
+        </Container>
+      </footer>
+    </div>
+  );
+}
