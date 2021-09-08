63bec82f03382811f63d701ea1bb8eb898f97a44
Neenus Gabriel <hello@neenus.com>
2021-09-07 21:17:31 -0400

    fix(footer): update footer link url

--- src/components/footer/footer.component.jsx
+++ src/components/footer/footer.component.jsx
@@ -8,7 +8,7 @@ function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary">
       {"Copyright © "}
-      <Link color="inherit" href="https://material-ui.com/">
+      <Link color="inherit" href="https://nraccounting.ca/">
         wwww.nraccounting.ca
       </Link>{" "}
       {new Date().getFullYear()}
